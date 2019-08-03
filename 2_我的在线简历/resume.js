0c2b352f80b8b8fd7b1850f68c23b296462c2baf
flysara <1395768483@qq.com>
2019-08-03 21:26:46 +0800

    表单样式练习

--- 2_我的在线简历/resume.js
+++ 2_我的在线简历/resume.js
@@ -3,10 +3,10 @@ document.getElementById("imag").onclick=function(){
 	var r=confirm("想要了解我的兴趣爱好吗？")
 	if(r==true)
 		{
-			document.write("我的兴趣爱好是唱歌、跳舞、听歌、看书、看电影、旅行、摄影，我对一切新事物都很好奇，所以我是一个很爱学习新东西的好奇宝宝")
+			document.write("我的兴趣爱好是唱歌、跳舞、听歌、看书、看电影、旅行、摄影，我对一切新事物都很好奇，所以我是一个很爱学习新东西的好奇宝宝");
 		}
 	else{
-		document.write("相信你了解我以后会很喜欢我的哦~")
+		document.write("相信你了解我以后会很喜欢我的哦~");
 	}
 }
 
